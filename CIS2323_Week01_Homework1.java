4213928e0aa1819be41d91824d097d025cabaf77
Schogan <navy15@gmail.com>
2018-08-25 20:23:28 -0500

    Homework1 complete

---
+++ CIS2323_Week01_Homework1.java
@@ -0,0 +1,13 @@
+public class CIS2323_Week01_Homework1 {
+	public static void main(String[] args){
+		System.out.println("============================================================================");
+		System.out.println("Logan Schanbacher");
+		System.out.println("Desktop Support, GC Services - Oklahoma City");
+		System.out.println("");
+		System.out.println("8121 NW Expressway			p: (405)621-6499");
+		System.out.println("Oklahoma City, OK			e: logan.schanbacher@gcserv.com");
+		System.out.println("					g: github.com/schogan");
+		System.out.println("============================================================================");
+	}
+}
+
